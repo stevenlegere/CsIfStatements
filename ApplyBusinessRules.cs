3a508124c1b9d22ea36861e68d5ab981319689b1
stevenlegere <stevenmarklegere@gmail.com>
2024-02-13 21:19:02 +0000

    Ready for challenge

--- ApplyBusinessRules.cs
+++ ApplyBusinessRules.cs
@@ -4,6 +4,14 @@ public class ApplyBusinessRules
 {
     public static void PrintToConsole()
     {
-        Console.WriteLine("Printing from ApplyBusinessRules.cs");
+Console.WriteLine("Printing from ApplyBusinessRules.cs");
+
+// https://learn.microsoft.com/en-us/training/modules/csharp-if-elseif-else/4-challenge
+
+Random random = new Random();
+int daysUntilExpiration = random.Next(12);
+int discountPercentage = 0;
+
+
     }
 }
