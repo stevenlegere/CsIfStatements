83f317610d915ada13ec6f3c16d3e2a17f5e7682
stevenlegere <stevenmarklegere@gmail.com>
2024-02-17 15:22:47 +0000

    working with arrays and foreach

---
+++ ArraysAndForeach/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+// new operator creates a new instance of the array
+        // First set of square brankets [] declares the variable as an array
+        // Second set of square brackets [] specifies the number of elements in the array
+    
+    // Assign Order ID values to your array
